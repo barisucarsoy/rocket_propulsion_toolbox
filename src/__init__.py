973f96ebd0e426356a539d366251f4976701cfa0
Barış Uçarsoy <barisucarsoy@outlook.com>
2025-07-06 19:02:52 +0200

    some stuff

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-# Make the loaded configuration data available directly when the package is imported.
-from .config_loader import config_data
-
-# Optional: You might want to add an __all__ if you want to control
-# what 'from src import *' imports, though explicit imports are often preferred.
-__all__ = ['config_data']
-
-# print(f"Package initialized. Configuration loaded into 'config_data'.")
